bda58a3b00a74a5789b53178c73072357f62e79d
bobby-webdev2021 <85532644+bobby-webdev2021@users.noreply.github.com>
2022-02-18 23:19:59 -0600

    Add files via upload

---
+++ Week4/axiosToDo1/index.js
@@ -0,0 +1,30 @@
+let mainDiv = document.querySelector("#mainDiv");
+
+let toDoHeader = document.createElement("h1");
+toDoHeader.textContent = "To-Do List!";
+mainDiv.appendChild(toDoHeader);
+
+axios.get("http://api.bryanuniversity.edu/bobbythomas/list")
+    .then(res => {
+        res.data.forEach((e, i) => {
+            let entryDiv = document.createElement("div");
+            let currToDoEntry = document.createElement("h2");
+            let detailList = document.createElement("ul");
+            let currDetailEntry = document.createElement("li");
+            currToDoEntry.textContent = res.data[i].name;
+            currDetailEntry.textContent = res.data[i].description;
+            entryDiv.setAttribute("class", "entry");
+            
+            if (res.data[i].isComplete == true)
+            {
+                currToDoEntry.setAttribute("style", "text-decoration: line-through");
+                currDetailEntry.setAttribute("style", "text-decoration: line-through");
+            }
+            
+            mainDiv.appendChild(entryDiv);
+            entryDiv.appendChild(currToDoEntry);
+            entryDiv.appendChild(detailList);
+            detailList.appendChild(currDetailEntry);
+        });
+    })
+    .catch(err => console.log(err))
---
+++ Week4/project2/monster.js
@@ -0,0 +1,63 @@
+// Ygo Control
+if (document.body.id == "digPage"){
+    document.querySelector("#digSubmit").addEventListener("click", () => {
+            if(document.querySelector("#digMain").childElementCount == 0){
+                let loading = true;
+                if(loading == true){
+                    document.querySelector("#digSubmit").innerHTML = "Loading...Please wait";
+                }
+            axios.get("https://digimoncard.io/api-public/search.php?n=Agumon")
+                .then(result =>{
+                    result.data.forEach(function(element, index){
+
+                        var entryDiv = document.createElement("div")
+                        var digName = document.createElement("h1")
+                        var digData = document.createElement("img")
+
+                        digName.textContent = result.data[index].name
+                        digData.setAttribute("src", result.data[index].image_url)
+                        document.querySelector("#digMain").appendChild(entryDiv)
+                        entryDiv.appendChild(digName)
+                        entryDiv.appendChild(digData)   
+                    })
+                loading = false;
+                if(loading == false){
+                    document.querySelector("#digSubmit").innerHTML = "Get All!";
+                }
+                })
+                .catch(err => console.log(err))
+        }
+    })
+}
+
+ // monster Control
+if (document.body.id == "monsterPage"){
+    document.querySelector("#monsterSubmit").addEventListener("click", () => {
+            if(document.querySelector("#monsterMain").childElementCount == 0){
+                let loading = true;
+                if(loading == true){
+                    document.querySelector("#monsterSubmit").innerHTML = "Loading...Please wait";
+                }
+            axios.get("https://mhw-db.com/ailments")
+                .then(result =>{
+                    result.data.forEach(function(element, index){
+
+                        var entryDiv = document.createElement("div")
+                        var monsterName = document.createElement("h1")
+                        var monsterData = document.createElement("h3")
+
+                        monsterName.textContent = result.data[index].name
+                        monsterData.textContent = result.data[index].description
+                        document.querySelector("#monsterMain").appendChild(entryDiv)
+                        entryDiv.appendChild(monsterName)
+                        entryDiv.appendChild(monsterData)   
+                    })
+                loading = false;
+                if(loading == false){
+                    document.querySelector("#monsterSubmit").innerHTML = "Get All!";
+                }
+                })
+                .catch(err => console.log(err))
+        }
+    })
+}
